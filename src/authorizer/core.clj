1d7020d3906940332613c2956336e6212b8a575d
nmkip <juan.ignacio848@gmail.com>
2019-10-28 17:00:57 -0300

    - doubled-transactions? and transactions-within-interval?

--- src/authorizer/core.clj
+++ src/authorizer/core.clj
@@ -42,24 +42,26 @@
 (defn card-active? [account]
   (:active-card account))
 
-(defn transactions-within-interval? [tx-1 tx-2]
-  (time/within-interval (instant (:time tx-1))
-                        (time/interval (instant (:time tx-2))
-                                       1 ChronoUnit/MINUTES)))
+(defn transactions-within-interval? [tx-1]
+  (fn [tx-2]
+    (time/within-interval (instant (:time tx-1))
+                                   (time/interval (instant (:time tx-2))
+                                                  1 ChronoUnit/MINUTES))))
 
 (defn transactions-within-interval [txs tx]
-  (filter #(transactions-within-interval? % tx) txs))
+  (filter (transactions-within-interval? tx) txs))
 
 (defn high-frequency-small-interval? [txs tx]
   (>= (count (transactions-within-interval txs tx)) 3))
 
-(defn similar-transactions? [tx-1 tx-2]
-  (and (= (:amount tx-1) (:amount tx-2))
-       (= (:merchant tx-1) (:merchant tx-2))))
+(defn similar-transactions? [tx-1]
+  (fn [tx-2]
+    (and (= (:amount tx-1) (:amount tx-2))
+         (= (:merchant tx-1) (:merchant tx-2)))))
 
 (defn doubled-transactions? [txs tx]
-  (< 1 (count (transduce (comp (filter #(similar-transactions? % tx))
-                               (filter #(transactions-within-interval? % tx)))
+  (< 1 (count (transduce (comp (filter (similar-transactions? tx))
+                               (filter (transactions-within-interval? tx)))
                          conj []
                          txs))))
 
